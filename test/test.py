5213915c5e026dd6b739847c51865d2635c41def
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-20 23:18:26 +0200

    kk

---
+++ test/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+
+class TestLambdaMethods(unittest.TestCase):
+    def test_delete_lambda(self):
+        self.assertEqual(True,True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
