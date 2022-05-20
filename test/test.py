95ffec37693ee41469a1ef2e8f20aa00fdb461fb
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-20 23:40:21 +0200

    kk

--- test/test.py
+++ test/test.py
@@ -1,12 +1,20 @@
 import unittest
 
+class TestStringMethods(unittest.TestCase):
 
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
-class TestLambdaMethods(unittest.TestCase):
-    def test_delete_lambda(self):
-        self.assertEqual(True,True)
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
-
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
