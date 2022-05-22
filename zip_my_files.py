69e310deaa39f5b816fb504095d3782b087a5d9c
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 23:15:39 +0200

    change in zipping

--- zip_my_files.py
+++ zip_my_files.py
@@ -2,12 +2,13 @@ from zipfile import ZipFile
 import os
 
 os.makedirs('zips', exist_ok=True)
-for f in os.listdir('code'):
+os.chdir('code')
+for f in os.listdir():
     file = f.split('.')
     if file[1] == 'py':
         try:
-            zf = ZipFile(os.path.join('zips', f'{file[0]}.zip'), mode='w')
-            zf.write(os.path.join('code', f))
+            zf = ZipFile(os.path.join('..','zips', f'{file[0]}.zip'), mode='w')
+            zf.write(f)
             zf.close()
         except Exception as e:
             print(e)
