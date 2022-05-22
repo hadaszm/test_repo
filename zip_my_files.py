cdeb87c8a09ceaaa8c38cdb70643d537726a6817
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 15:13:15 +0200

    added workflow

--- zip_my_files.py
+++ zip_my_files.py
@@ -1,13 +1,13 @@
 from zipfile import ZipFile
 import os
 
-
+os.makedirs('zips', exist_ok=True)
 for f in os.listdir('code'):
     file = f.split('.')
     if file[1] == 'py':
         try:
-            zf = ZipFile(os.path.join('zips',f'{file[0]}.zip'), mode='w')
-            zf.write(os.path.join('code',f))
+            zf = ZipFile(os.path.join('zips', f'{file[0]}.zip'), mode='w')
+            zf.write(os.path.join('code', f))
             zf.close()
         except Exception as e:
-            print(e, os.listdir)
+            print(e)
