b0a1a3e804bc19cda99ec0aaa09371669ede3514
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 23:28:27 +0200

    change in zipping

--- zip_my_files.py
+++ zip_my_files.py
@@ -5,6 +5,7 @@ os.makedirs('zips', exist_ok=True)
 os.chdir('code')
 for f in os.listdir():
     file = f.split('.')
+    
     if file[1] == 'py':
         try:
             zf = ZipFile(os.path.join('..','zips', f'{file[0]}.zip'), mode='w')
