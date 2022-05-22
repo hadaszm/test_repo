395036ed71cc7eb375bc569a829973371cd3c211
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 13:56:32 +0200

    small change

--- zip_my_files.py
+++ zip_my_files.py
@@ -4,6 +4,9 @@ import os
 for f in os.listdir('code'):
     file = f.split('.')
     if file[1] == 'py':
-        zf = ZipFile(os.path.join('zips', file[0], '.zip'), mode='w')
-        zf.write(os.path.join('code',f))
-        zf.close()
+        try:
+            zf = ZipFile(os.path.join('zips', file[0], '.zip'), mode='w')
+            zf.write(os.path.join('code',f))
+            zf.close()
+        except Exception as e:
+            print(e, os.listdir)
