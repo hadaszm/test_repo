63cfa156c7f98a95b1c4b4d96f71fde45da9cab3
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 13:46:28 +0200

    please work

---
+++ zip_my_files.py
@@ -0,0 +1,9 @@
+from zipfile import ZipFile
+import os
+
+for f in os.listdir('code'):
+    file = f.split('.')
+    if file[1] == 'py':
+        zf = ZipFile(f'zips\\{file[0]}.zip', mode='w')
+        zf.write(f'code\\{f}')
+        zf.close()
