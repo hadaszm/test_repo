4a78cab8ffcc92afdefaa1d956242ea943a66c2c
Gosia Hadasz <Ghadasz@interia.eu>
2022-05-22 13:55:54 +0200

    small change

--- zip_my_files.py
+++ zip_my_files.py
@@ -1,9 +1,9 @@
 from zipfile import ZipFile
 import os
 
-for f in os.listdir('..\\code'):
+for f in os.listdir('code'):
     file = f.split('.')
     if file[1] == 'py':
-        zf = ZipFile(f'..\\zips\\{file[0]}.zip', mode='w')
-        zf.write(f'..\\code\\{f}')
+        zf = ZipFile(os.path.join('zips', file[0], '.zip'), mode='w')
+        zf.write(os.path.join('code',f))
         zf.close()
